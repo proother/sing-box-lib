c29a3849f5d766fcad9ac99af60ebb05cec5b41d
proother <61898395+proother@users.noreply.github.com>
2025-03-27 12:53:53 +0000

    Libbox Release v1.11.6

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/proother/sing-box-lib/releases/download/v1.11.5/Libbox.xcframework.zip",
-      checksum: "6676aca61a47870c2210ef3774fd8030dad3957763ca729055a5110e440623d9"
+      url: "https://github.com/proother/sing-box-lib/releases/download/v1.11.6/Libbox.xcframework.zip",
+      checksum: "f23d280b4c428ce65b53c77a45e5fa8c8a4428484f8a3cdd5d49fda5bfde52de"
     )
   ]
 )
